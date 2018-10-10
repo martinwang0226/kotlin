ae7cdff3f1b63a49dd3aeb08e25c1d625634004d
marting.wang <mc3895q@dingtalk.com>
2018-10-10 15:22:32 +0800

    引入anko库

--- kotlin/app/src/main/java/e/martinwang/kotlin/MainActivity.kt
+++ kotlin/app/src/main/java/e/martinwang/kotlin/MainActivity.kt
@@ -2,11 +2,16 @@ package e.martinwang.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.longToast
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        tv_hello.setText("Hello kotlin")
+        tv_hello.text = "Hello"
     }
 }
