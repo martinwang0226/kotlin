5f95fd6f01eba307a4da9e5c8ca2a43b0273d5fc
marting.wang <mc3895q@dingtalk.com>
2018-10-10 18:16:43 +0800

    控制语句

---
+++ kotlin/app/src/main/java/e/martinwang/kotlin/controlFlow/CaseActivity.kt
@@ -0,0 +1,16 @@
+package e.martinwang.kotlin.controlFlow
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import e.martinwang.kotlin.R
+
+/**
+ * Created by martinwang on 2018/10/10.
+ */
+class CaseActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
