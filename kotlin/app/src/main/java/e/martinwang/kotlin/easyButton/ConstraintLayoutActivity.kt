f46737a25224330f7b8237af2a9176035060d677
marting.wang <mc3895q@dingtalk.com>
2018-10-12 18:31:07 +0800

    基础控件

--- kotlin/app/src/main/java/e/martinwang/kotlin/easyButton/ConstraintLayoutActivity.kt
+++ kotlin/app/src/main/java/e/martinwang/kotlin/easyButton/ConstraintLayoutActivity.kt
@@ -2,7 +2,9 @@ package e.martinwang.kotlin.easyButton
 
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
+import android.transition.TransitionManager
 import android.widget.TextView
 import e.martinwang.kotlin.R
 import kotlinx.android.synthetic.main.layout_activity_constraint.*
@@ -19,7 +21,10 @@ class ConstraintLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btn_move_soft.setOnClickListener{
+            TransitionManager.beginDelayedTransition(cl_content)
+            moveView()
+        }
     }
 
     private fun addNewView() {
@@ -42,7 +47,13 @@ class ConstraintLayoutActivity : AppCompatActivity() {
         cl_content.addView(tv)
     }
 
-    private fun moveView(){
-//        val margin = dip((if(isMoved)) 200 else 20)
+    private fun moveView() {
+        val margin = dip((if (isMoved) 200 else 20).toFloat())
+        val set = ConstraintSet()
+        set.clone(cl_content)
+        set.connect(tv_first.id, ConstraintSet.END, cl_content.id, ConstraintSet.END, margin)
+        set.applyTo(cl_content)
+        isMoved = !isMoved
     }
+
 }
---
+++ kotlin/app/src/main/java/e/martinwang/kotlin/easyButton/TextMarqueeActivity.kt
@@ -0,0 +1,16 @@
+package e.martinwang.kotlin.easyButton
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by martinwang on 2018/10/12.
+ */
+class TextMarqueeActivity : AppCompatActivity() {
+    private var bPause = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
